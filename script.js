3828bd071b9f3ea532cf52a8f5d701b0b6a21682
Tanishka Talwar <tanishkatalwar18@gmail.com>
2022-05-26 20:03:50 +0530

    add skeleton function and add eventlistener to buttons

--- script.js
+++ script.js
@@ -8,6 +8,31 @@ function Book(name, author, pages, readStatus){
     this.readStatus = readStatus
 }
 
+// function to add a new book object to library
 function addBookToLibrary(){
-    
+
 }
+
+// function to show up form
+function showForm(){
+
+}
+
+// function to delete card and book object
+function deleteBook(){
+
+}
+
+// function to add new card to the library
+function addCard(){
+
+}
+
+// buttons
+let addButton = document.querySelector(".add-button");
+let formSubmitButton = document.querySelector(".submit-form");
+let cardDeleteButton = document.querySelector(".delete");
+
+addButton.addEventListener("click", showForm);
+formSubmitButton.addEventListener("click", addBookToLibrary);
+cardDeleteButton.addEventListener("click", deleteBook);
