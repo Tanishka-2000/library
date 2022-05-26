87e3ec24c785e7c364072dca598329ab5daf5365
Tanishka Talwar <tanishkatalwar18@gmail.com>
2022-05-26 21:09:47 +0530

    add function to display and hide form

--- script.js
+++ script.js
@@ -10,12 +10,17 @@ function Book(name, author, pages, readStatus){
 
 // function to add a new book object to library
 function addBookToLibrary(){
+    container.style.opacity = 1;
+    form.style.display = "none";
 
+    //after adding new book object to array
+    addCard();
 }
 
 // function to show up form
 function showForm(){
-
+    container.style.opacity = 0;
+    form.style.display = "block";
 }
 
 // function to delete card and book object
@@ -29,10 +34,18 @@ function addCard(){
 }
 
 // buttons
-let addButton = document.querySelector(".add-button");
+let addButton = document.querySelector(".add-book");
 let formSubmitButton = document.querySelector(".submit-form");
-let cardDeleteButton = document.querySelector(".delete");
+let cardDeleteButton = document.querySelectorAll(".delete");
 
+// adding event listeners to buttons
 addButton.addEventListener("click", showForm);
 formSubmitButton.addEventListener("click", addBookToLibrary);
-cardDeleteButton.addEventListener("click", deleteBook);
+
+for (let i = 0; i < cardDeleteButton.length; i++) {
+    cardDeleteButton[i].addEventListener("click", deleteBook);
+}
+
+// divs
+let container = document.querySelector(".container");
+let form = document.querySelector("form");
