cbb0c508b3359b8bf356c0c6680f640e501d397a
Tanishka Talwar <tanishkatalwar18@gmail.com>
2022-05-26 14:52:54 +0530

    write initial code to script file

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+let myLibrary = [];
+
+function Book(name, author, pages, readStatus){
+    this.name = name
+    this.author = author
+    this.pages = pages
+    this.readStatus = readStatus
+}
+
+function addBookToLibrary(){
+    
+}
